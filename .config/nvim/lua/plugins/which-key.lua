acd8f5eaed7d0026da7ca6c8029e6f7763fd01fd
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2025-05-24 16:03:55 -0400

    Which-keys

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    win = {
+      width = { min = 30, max = 60 },
+      col = -1,
+      row = -1,
+    }
+  }
+}
+
