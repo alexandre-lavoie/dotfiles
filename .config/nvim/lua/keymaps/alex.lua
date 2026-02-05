c0b679627619498c78d891cd617c17adec07edc1
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2026-02-05 17:04:08 -0500

    Update

--- .config/nvim/lua/keymaps/alex.lua
+++ .config/nvim/lua/keymaps/alex.lua
@@ -9,3 +9,4 @@ vim.keymap.set("n", "<Space><Right>", "<C-i>", { noremap = true, desc = "Go to n
 vim.keymap.set("n", "<Space><Up>", function() Snacks.picker.lsp_definitions() end, { noremap = true, desc = "Go up to definition" })
 vim.keymap.set("n", "<Space><Down>", function() Snacks.picker.lsp_references() end, { noremap = true, desc = "Go down to references" })
 vim.keymap.set("n", "<Space><Space><Down>", function() Snacks.picker.lsp_implementations() end, { noremap = true, desc = "Go down to implementations" })
+
